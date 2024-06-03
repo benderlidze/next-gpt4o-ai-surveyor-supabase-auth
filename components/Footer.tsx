5911e6f280b1f96e334e10500a64051b176d5a5f
Serg <benderlio@gmail.com>
2024-06-03 17:00:46 +0200

    TawkMessengerReact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,7 @@
+"use client"
+// @ts-ignore
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+
 export const Footer = () => {
   return (
     <footer className="align-middle items-center flex-row w-full border-t p-4 flex justify-between ">
@@ -11,6 +15,12 @@ export const Footer = () => {
           The Requirement List
         </a>
       </div>
+
+      <TawkMessengerReact
+        propertyId="649051de94cf5d49dc5e99cd"
+        widgetId="1h39rvjn7"
+      />
+
       <div className="flex-1 flex items-center text-center flex-col ">
         <div>Trusted by the leading property consultancies in the UK</div>
         <img src="images/logos.png" alt="" className="max-w-[200px]" />
